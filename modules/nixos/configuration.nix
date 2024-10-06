b073bef5941a294b001cac48077f1e0ae7f29e4f
dauliac <dauliac@protonmail.com>
2024-10-06 16:58:11 +0200

    feat: enable flake

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -11,4 +11,21 @@
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICB4/43J0s1YooaIDSVhq0tLKRaEJpQsUZ1ttzCJziuJ dauliac@nixos'' ];
   system.stateVersion = "23.11";
+  nix = {
+    settings = {
+      system-features = [
+        "benchmark"
+        "big-parallel"
+        "nixos-test"
+      ];
+      experimental-features = ["nix-command" "flakes"];
+    };
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      persistent = true;
+      dates = "012:15";
+      options = "-d";
+    };
+  };
 }

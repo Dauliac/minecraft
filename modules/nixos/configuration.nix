67d92871e4e1416c0ae1ce4bcab42076b33e128a
dauliac <dauliac@protonmail.com>
2025-06-14 22:01:37 +0200

    feat: update config

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -39,38 +39,44 @@
     vim
   ];
   networking.firewall.enable = true;
-  services.minecraft-servers.servers.leschiens = {
+  services.minecraft-servers = {
     enable = true;
     eula = true;
     package = pkgs.paperServers.paper-1_21_4;
-    openFirewall = true;
-    dataDir = "/srv/minecraft";
-    whitelist = {
-      dauliac = "ea017fcd-02a7-4b04-9895-7e11eb58c529";
-      aaronky = "69d16278-23bb-491b-b7a3-11c0c17d9a73";
-      walalak = "f70899d1-3d0e-4d70-9a68-bf535c196eea";
-    };
-    symlinks = {
-      mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
-        C2ME = builtins.fetchurl {
-          url = "https://cdn.modrinth.com/data/1u6JkXh5/versions/txgouwkE/worldedit-bukkit-7.3.14.jar";
-          sha512 = "562c87a50f380c6cd7312f90b957f369625b3cf5f948e7bee286cd8075694a7206af4d0c8447879daa7a3bfe217c5092a7847247f0098cb1f5417e41c678f0c1";
-        };
-      });
-    };
-    serverProperties = {
-      motd = "Les chiens";
-      online-mode = false;
-      white-list = true;
-      max-players = 4;
-      difficulty = "normal";
-      gamemode = "survival";
-      # enable-rcon = true;
-      # "rcon.password" = "longview";
-      view-distance = 64;
-      simulation-distance = 32;
-      spawn-protection = 5;
-      allow-flight = true;
+
+    leschiens = {
+      enable = true;
+      eula = true;
+      package = pkgs.paperServers.paper-1_21_4;
+      openFirewall = true;
+      dataDir = "/srv/minecraft";
+      whitelist = {
+        dauliac = "ea017fcd-02a7-4b04-9895-7e11eb58c529";
+        aaronky = "69d16278-23bb-491b-b7a3-11c0c17d9a73";
+        walalak = "f70899d1-3d0e-4d70-9a68-bf535c196eea";
+      };
+      symlinks = {
+        mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
+          C2ME = builtins.fetchurl {
+            url = "https://cdn.modrinth.com/data/1u6JkXh5/versions/txgouwkE/worldedit-bukkit-7.3.14.jar";
+            sha512 = "562c87a50f380c6cd7312f90b957f369625b3cf5f948e7bee286cd8075694a7206af4d0c8447879daa7a3bfe217c5092a7847247f0098cb1f5417e41c678f0c1";
+          };
+        });
+      };
+      serverProperties = {
+        motd = "Les chiens";
+        online-mode = false;
+        white-list = true;
+        max-players = 4;
+        difficulty = "normal";
+        gamemode = "survival";
+        # enable-rcon = true;
+        # "rcon.password" = "longview";
+        view-distance = 64;
+        simulation-distance = 32;
+        spawn-protection = 5;
+        allow-flight = true;
+      };
     };
   };
 }

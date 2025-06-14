139943790099b9bdc8e4754c510ee7b91a141ee3
dauliac <dauliac@protonmail.com>
2025-06-14 22:03:21 +0200

    feat: update config

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -42,11 +42,11 @@
   services.minecraft-servers = {
     enable = true;
     eula = true;
+    openFirewall = true;
 
     servers.leschiens = {
       enable = true;
       package = pkgs.paperServers.paper-1_21_4;
-      openFirewall = true;
       whitelist = {
         dauliac = "ea017fcd-02a7-4b04-9895-7e11eb58c529";
         aaronky = "69d16278-23bb-491b-b7a3-11c0c17d9a73";
